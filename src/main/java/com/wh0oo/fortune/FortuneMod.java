fae06b4695ebc202d7238893e1f265d4c50373f9
wh0oo <45668419+wh0oo@users.noreply.github.com>
2025-07-12 09:58:40 -0500

    Create FortuneMod.java
    send a fortune to a player when they join

---
+++ src/main/java/com/wh0oo/fortune/FortuneMod.java
@@ -0,0 +1,21 @@
+package com.wh0oo.fortune;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+
+public class FortuneMod implements ModInitializer {
+    public static final String MOD_ID = "fortune";
+
+    @Override
+    public void onInitialize() {
+        // Send a random fortune on player join
+        ServerPlayConnectionEvents.JOIN.register((ServerPlayNetworkHandler handler, PacketSender sender, MinecraftServer server) -> {
+            ServerPlayerEntity player = handler.getPlayer();
+            String fortune = FortuneManager.getRandomFortune();
+            player.sendMessage(Text.literal("§6Fortune: §r" + fortune), false);
+        });
+    }
+}
